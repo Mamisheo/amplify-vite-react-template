ec6aa2e8104c5f2228435a7e0f118b498937a1d9
Mamisheo <taishavargas3@gmail.com>
2025-09-04 12:46:17 +0000

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from '@aws-amplify/ui-react';
-import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
    const { signOut } = useAuthenticator();
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+
 
   return (
     <main>
