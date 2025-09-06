38ae088ed2ee22341093e8346af2a87be8cad3d7
Mamisheo <taishavargas3@gmail.com>
2025-09-06 03:20:03 +0000

    maintenance commit message

--- amplify-ui-demo/src/main.jsx
+++ amplify-ui-demo/src/main.jsx
@@ -1,7 +1,20 @@
+import '@aws-amplify/ui-react/styles.css';
+
+import { Amplify } from 'aws-amplify';
+import { ThemeProvider } from '@aws-amplify/ui-react';
+
+import amplifyconfig from './amplifyconfiguration.json';
+import studioTheme from './ui-components/studioTheme';
+Amplify.configure(amplifyconfig);
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+<ThemeProvider theme={studioTheme}>
+  <App />
+</ThemeProvider>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
